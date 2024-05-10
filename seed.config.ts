999cb0286584919b6f548a07c76f21e562c9f35c
Parker Nilson <parker.todd.nilson@gmail.com>
2024-05-09 21:58:00 -0700

    Set up snaplet seed data

--- seed.config.ts
+++ seed.config.ts
@@ -7,14 +7,4 @@ export default defineConfig({
     const client = postgres("postgresql://postgres:postgres@127.0.0.1:54322/postgres");
     return new SeedPostgres(client);
   },
-  select: [
-    // We don't alter any extensions tables that might be owned by extensions
-    "!*",
-    // We want to alter all the tables under public schema
-    "public*",
-    // We also want to alter some of the tables under the auth schema
-    "auth.users",
-    "auth.identities",
-    "auth.sessions",
-  ]
 });
--- seed.ts
+++ seed.ts
@@ -6,11 +6,49 @@
  */
 import { createSeedClient } from "@snaplet/seed";
 
+const dan = {
+  email: "danexample@gmail.com",
+  id: "b77f5aac-8f78-47a3-8a56-a52fa7363bee",
+  encrypted_password: "$2a$10$zefh5Jv7MWpgb5NVucKGgel/NZPpBObTXz7qv0EswwoQb9ZR5q9Va"
+};
+
+const sabrina = {
+  email: "sabrinatest@mail.com",
+  id: "11bd719b-8370-4c7b-b4d0-d3463c61a72f",
+  encrypted_password: "$2a$10$hD4pRadLBGkQ8F72EVv2L.F6PG/x5GYCGwPluOpkYKXz0MzEy5P4S"
+};
+
 const main = async () => {
   const seed = await createSeedClient({ dryRun: true });
 
   // Truncate all tables in the database
   await seed.$resetDatabase();
+
+  // Seed the database with 10 users
+  await seed.users([
+    {
+      ...dan,
+      goals: (x) =>
+        x(8, ({ index }) => ({
+          entries: (x) => x(5),
+          share_records: [
+            {
+              viewer: sabrina.id,
+              share_responses: [{
+                accepted: Math.random() > 0.5 ? true : false,
+              }],
+            },
+          ],
+        })),
+    },
+    {
+      ...sabrina,
+      goals: (x) =>
+        x(5, {
+          entries: (x) => x(5),
+        }),
+    },
+  ]);
 
   process.exit();
 };
